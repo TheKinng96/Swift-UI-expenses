e50805a39e6005bcdc7f3e1b3d8916aa40ed4de3
Yap Feng Yuan <fengyuan.yap@gmail.com>
2022-05-02 10:55:19 +0900

    Initial Commit

---
+++ expenses/expensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensesApp.swift
+//  expenses
+//
+//  Created by Feng Yuan Yap on 2022/05/02.
+//
+
+import SwiftUI
+
+@main
+struct expensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
